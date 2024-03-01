bfc6c251c0df256aad7b1a19342cc83fb4e241dd
DysterFall <150154654+DysterFall@users.noreply.github.com>
2024-03-01 18:16:46 +0100

    J-2

--- gestion_utilisateur.py
+++ gestion_utilisateur.py
@@ -4,99 +4,78 @@ import string
 import sqlite3
 
 class User:
-    def __init__(self, first_name, last_name, email, phone, project_code, role):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.email = email
-        self.phone = phone
-        self.project_code = project_code
-        self.role = role
-        self.password = self.generate_password()
-        self.login = self.generate_login()
+    class UserManager:
+        ROLES = ['chercheur', 'medecin', 'commercial', 'assistant']
 
-    def generate_password(self):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(characters) for i in range(12))
-        return hashlib.sha256(password.encode()).hexdigest()
+        def __init__(self, db_file):
+            self.db_file = db_file
+            self.create_table()
 
-    def generate_login(self):
-        return self.__first_name[0].lower() + self.__last_name.lower()
-
-    def get_last_name(self):
-        return self.__last_name
-    
-    def get_name(self):
-        return self.__first_name
-
-    def get_login(self):
-        return self.login
-
-class UserManager:
-    def __init__(self, db_file):
-        self.db_file = db_file
-        self.create_table()
-
-    def create_table(self):
-        conn = sqlite3.connect(self.db_file)
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users
-                     (first_name TEXT, last_name TEXT, email TEXT PRIMARY KEY, phone TEXT, project_code TEXT, role TEXT, password TEXT)''')
-        conn.commit()
-        conn.close()
-
-    def create_user(self, first_name, last_name, email, phone, project_code, role):
-        password = self.generate_password()  # Générer le mot de passe
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()  # Hacher le mot de passe
-        user = User(first_name, last_name, email, phone, project_code, role)
-        conn = sqlite3.connect(self.db_file)
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)",
-                    (user.get_name(), user.get_last_name(), user.email, user.phone, user.project_code, user.role, hashed_password))  # Utiliser le mot de passe haché
-        conn.commit()
-        conn.close()
-        return user
-
-    def generate_password(self):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(characters) for i in range(12))
-        return password
-
-    def modify_user(self, email, **kwargs):
-        try:
+        def create_table(self):
             conn = sqlite3.connect(self.db_file)
             cursor = conn.cursor()
-            update_query = f"UPDATE users SET {', '.join([f'{key}=?' for key in kwargs.keys()])} WHERE email=?"
-            cursor.execute(update_query, tuple(kwargs.values()) + (email,))
+            cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                         (first_name TEXT, last_name TEXT, email TEXT PRIMARY KEY, phone TEXT, project_code TEXT, role TEXT, password TEXT)''')
             conn.commit()
-            print("Utilisateur modifié avec succès.")
-        except sqlite3.Error as e:
-            print("Erreur lors de la modification de l'utilisateur :", e)
-        finally:
             conn.close()
 
+        def create_user(self, first_name, last_name, email, phone, project_code, role):
+            if role.lower() not in self.ROLES:
+                print("Erreur : Le rôle spécifié n'est pas valide.")
+                return None
 
-    def delete_user(self, email):
-        try:
+            password = self.generate_password()  # Générer le mot de passe
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()  # Hacher le mot de passe
+            user = User(first_name, last_name, email, phone, project_code, role)
             conn = sqlite3.connect(self.db_file)
             cursor = conn.cursor()
-            cursor.execute("DELETE FROM users WHERE email=?", (email,))
+            cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)",
+                        (user.get_name(), user.get_last_name(), user.email, user.phone, user.project_code, user.role, hashed_password))  # Utiliser le mot de passe haché
             conn.commit()
-            print("Utilisateur supprimé avec succès.")
-        except sqlite3.Error as e:
-            print("Erreur lors de la suppression de l'utilisateur :", e)
-        finally:
             conn.close()
+            return user
+
+        def generate_password(self):
+            characters = string.ascii_letters + string.digits + string.punctuation
+            password = ''.join(random.choice(characters) for i in range(12))
+            return password
+
+        def modify_user(self, email, **kwargs):
+            try:
+                conn = sqlite3.connect(self.db_file)
+                cursor = conn.cursor()
+                update_query = f"UPDATE users SET {', '.join([f'{key}=?' for key in kwargs.keys()])} WHERE email=?"
+                cursor.execute(update_query, tuple(kwargs.values()) + (email,))
+                conn.commit()
+                print("Utilisateur modifié avec succès.")
+            except sqlite3.Error as e:
+                print("Erreur lors de la modification de l'utilisateur :", e)
+            finally:
+                conn.close()
+
+
+        def delete_user(self, email):
+            try:
+                conn = sqlite3.connect(self.db_file)
+                cursor = conn.cursor()
+                cursor.execute("DELETE FROM users WHERE email=?", (email,))
+                conn.commit()
+                print("Utilisateur supprimé avec succès.")
+            except sqlite3.Error as e:
+                print("Erreur lors de la suppression de l'utilisateur :", e)
+            finally:
+                conn.close()
 
 
 
-    def display_users(self):
-        conn = sqlite3.connect(self.db_file)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(f"Nom: {row[0]}, Nom de famille: {row[1]}, Email: {row[2]}, Téléphone: {row[3]}, Code de projet: {row[4]}, Rôle: {row[5]}, Mot de passe: {row[6]}")
-        conn.close()
+        def display_users(self):
+            conn = sqlite3.connect(self.db_file)
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM users")
+            rows = cursor.fetchall()
+            for row in rows:
+                print(f"Nom: {row[0]}, Nom de famille: {row[1]}, Email: {row[2]}, Téléphone: {row[3]}, Code de projet: {row[4]}, Rôle: {row[5]}, Mot de passe: {row[6]}")
+            conn.close()
 
 # Fonction pour afficher le menu et traiter les choix de l'utilisateur
 def afficher_menu(user_manager):
@@ -121,12 +100,17 @@ def login(username, password):
 
         if row:
             role, hashed_password = row
-            if hashed_password == hashlib.sha256(password.encode()).hexdigest():
-                print("Connexion réussie.")
-                return role  # Retourner le rôle de l'utilisateur
-            else:
-                print("Mot de passe incorrect.")
-                return None
+            attempt = 0
+            while attempt < 3:
+                if hashed_password == hashlib.sha256(password.encode()).hexdigest():
+                    print("Connexion réussie.")
+                    return role 
+                else:
+                    attempt += 1
+                    print("Mot de passe incorrect. Tentative", attempt, "sur 3.")
+                    password = input("Veuillez réessayer le mot de passe : ")
+            print("Trop de tentatives infructueuses. Veuillez réessayer plus tard.")
+            return None
         else:
             print("Utilisateur non trouvé.")
             return None
@@ -136,6 +120,7 @@ def login(username, password):
     finally:
         conn.close()
 
+
 # Utilisation de la fonction pour se connecter
 username = input("Nom d'utilisateur : ")
 password = input("Mot de passe : ")
@@ -143,21 +128,17 @@ password = input("Mot de passe : ")
 user_role = login(username, password)
 
 if user_role == 'admin':
-    user_manager = UserManager('user.db')
+    user_manager = User.UserManager('user.db')
     # Reste du code pour l'interface utilisateur administrateur
 elif user_role == 'user':
     print("Bienvenue utilisateur normal.")
     # Code pour l'interface utilisateur normal
 else:
-   
-
     print("Échec de la connexion.")
 
 # Utilisation de la fonction pour se connecter en tant qu'administrateur
-   
-
 if user_role == 'admin':
-    user_manager = UserManager('user.db')
+    user_manager = User.UserManager('user.db')
 
     while True:
         choix = afficher_menu(user_manager)
@@ -172,7 +153,10 @@ if user_role == 'admin':
             role = input("Entrez le rôle de l'utilisateur : ")
 
             nouvel_utilisateur = user_manager.create_user(first_name, last_name, email, phone, project_code, role)
-            print("Utilisateur créé avec succès.")
+            if nouvel_utilisateur:
+                print("Utilisateur créé avec succès.")
+            else:
+                print("L'utilisateur n'a pas été créé en raison d'un rôle non valide.")
 
 
         elif choix == '2':
@@ -210,10 +194,6 @@ if user_role == 'admin':
             else:
                 print("Modification annulée.")
 
-
-
-
-
         elif choix == '3':
             email = input("Entrez l'email de l'utilisateur à supprimer : ")
             confirmation = input("Êtes-vous sûr de vouloir supprimer cet utilisateur ? (oui/non) : ")
@@ -223,12 +203,9 @@ if user_role == 'admin':
             else:
                 print("Suppression annulée.")
 
-
         elif choix == '4':
             user_manager.display_users()
 
         elif choix == '5':
             print("Quitting...")
             break  # Sortir de la boucle while et terminer le programme
-
-
